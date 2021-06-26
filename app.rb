ed26142482bccc675d92a2f96d7c9e18528467e5
Hannah M <hannahemalm616@gmail.com>
2021-06-26 22:52:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,10 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 get '/' do 
+<<<<<<< HEAD
   erb :index
 end 
 
 get "/info" do 
   erb :info
+=======
+  "<h1>Hello World></h1>"
+>>>>>>> 3ea47138c27185db85df120bd96ea3d9e8947b43
 end 
 end
